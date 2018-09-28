94170ec5befc2eb05296b49ab31a7676539523cc
SLandgra <slandgra@ucsd.edu>
2018-09-27 21:34:21 -0700

    allow cors requests

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const app = require('express')();
 const logger = require('morgan');
 const routes =  require('./routes');
 
-
-
 app.use('/', routes);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
